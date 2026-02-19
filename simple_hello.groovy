928f286d2d216550fa314402406b2ae786b7e598
ariya <ariyakrishnan.s@intelizign.com>
2026-02-19 10:18:58 +0530

    n

--- simple_hello.groovy
+++ simple_hello.groovy
@@ -1,10 +1,19 @@
 pipeline {
     agent any
+    parameters {
+        string(name: 'ENV', defaultValue: 'DEV', description: 'Environment (DEV, QA, etc)')
+    }
     stages {
         stage('Hello') {
             steps {
                 script {
-                    println('Hello World')
+                    if (params.ENV == 'DEV') {
+                        println('This is DEV environment')
+                    } else if (params.ENV == 'QA') {
+                        println('This is QA environment')
+                    } else {
+                        println("This is ${params.ENV} environment")
+                    }
                 }
             }
         }
